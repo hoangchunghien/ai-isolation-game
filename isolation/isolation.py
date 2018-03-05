d9556f8f4964b6a662a9cc412527e6962080be43
Hoang Chung Hien <hoangchunghien@gmail.com>
2018-03-05 09:17:59 +0700

    feat: Add Board class for Isolation game

---
+++ isolation/isolation.py
@@ -0,0 +1,14 @@
+"""
+This file contains the `Board` class, which implements the rules for the
+game Isolation as described in lecture, modified so that the players move
+like knights in chess rather than queens.
+"""
+import random
+import timeit
+from copy import copy
+
+TIME_LIMIT_MILLIS = 150
+
+
+class Board(object):
+    pass
